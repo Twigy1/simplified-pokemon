d3bacb1d034162c62df261668e030a6d98ee6f7d
Twigy1 <twigy1@csu.fullerton.edu>
2024-01-18 18:17:18 -0800

    commit

--- src/Card.js
+++ src/Card.js
@@ -2,16 +2,29 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Card = ({poke}) => {
-    let pokePicture = "";
+  const [loading,setLoading] = useState(true)
+  const [pokePic, setPokePic] = useState("")
+  
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(poke.url, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setPokePic(res.data.sprites.other["official-artwork"].front_default)
+      
 
-     axios.get(poke.url).then(res => {
-         pokePicture = res.data.sprites.other["official-artwork"].front_default
-     })
+    })
+
+    return () => cancel()
+  },[])
+
 
   return (
     <>
         <div key={poke.name}>{poke.name}</div>
-        <img src={pokePicture}/>
+        <img src={pokePic} alt="image not available"/>
     </>
   )
 }
