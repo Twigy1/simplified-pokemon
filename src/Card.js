25ae78f6b63eb36b1acf5258bf2b1e4d00ee8ecc
Twigy1 <twigy1@csu.fullerton.edu>
2024-01-19 17:28:01 -0800

    commit

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const capitalizeWords = (str) => {
+  return str.replace(/\b\w/g, (match) => match.toUpperCase());
+};
+
 const Card = ({poke}) => {
   const [loading,setLoading] = useState(true)
   const [pokePic, setPokePic] = useState("")
+  const [pokeHP, setPokeHP] = useState()
+  const [pokeATK, setPokeATK] = useState()
+  const [pokeDEF, setPokeDEF] = useState()
+  const [pokeSPA, setPokeSPA] = useState()
+  const [pokeSPD, setPokeSPD] = useState()
+  const [pokeSPE, setPokeSPE] = useState()
+  const [pokeAbility, setPokeAbility] = useState([])
+  const [pokeTypes, setPokeTypes] = useState([])
   
   useEffect(() => {
     setLoading(true)
@@ -13,7 +25,14 @@ const Card = ({poke}) => {
     }).then(res => {
       setLoading(false)
       setPokePic(res.data.sprites.other["official-artwork"].front_default)
-      
+      setPokeHP(res.data.stats[0].base_stat)
+      setPokeATK(res.data.stats[1].base_stat)
+      setPokeDEF(res.data.stats[2].base_stat)
+      setPokeSPA(res.data.stats[3].base_stat)
+      setPokeSPD(res.data.stats[4].base_stat)
+      setPokeSPE(res.data.stats[5].base_stat)
+      setPokeAbility(res.data.abilities)
+      setPokeTypes(res.data.types)
 
     })
 
@@ -23,8 +42,24 @@ const Card = ({poke}) => {
 
   return (
     <>
-        <div key={poke.name}>{poke.name}</div>
+        <div key={poke.name}>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</div>
         <img src={pokePic} alt="image not available"/>
+        <p>HP: {pokeHP}</p>
+        <p>ATK: {pokeATK}</p>
+        <p>DEF: {pokeDEF}</p>
+        <p>SPA: {pokeSPA}</p>
+        <p>SPD: {pokeSPD}</p>
+        <p>SPE: {pokeSPE}</p>
+        <ul>Abilities: {pokeAbility.map((ability, index) => (
+          <li key={index}>
+            {capitalizeWords(ability.ability.name)}
+          </li>))}
+        </ul>
+        <ul>Typing: {pokeTypes.map((type, index) => (
+          <li key={index}>
+            {capitalizeWords(type.type.name)}
+          </li>))}
+        </ul>
     </>
   )
 }
