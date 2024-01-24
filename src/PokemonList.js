ed5ea2f5b77bca9143d154f4f1e5eafbe39d2d69
Twigy1 <twigy1@csu.fullerton.edu>
2024-01-23 20:44:19 -0800

    commit

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import PokeCard from './PokeCard'
 
 export default function PokemonList({pokemon}) {
   
 
   return (
-    <div>
-      {pokemon.map(poke => (
-        <>
-          <center>
-            <PokeCard poke={poke}/>
-          </center>
-        </>
-      ))}
-    </div>
+    <Box>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {pokemon.map(poke => (
+          <>
+            <Grid item xs={2} sm={4} md={4}>
+              <PokeCard poke={poke}/>
+            </Grid>
+          </>
+        ))}
+      </Grid>
+    </Box>
   )
 }
